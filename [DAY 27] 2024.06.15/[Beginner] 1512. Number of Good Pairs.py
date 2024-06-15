6fb081885008e8343a4debd311261c4baf500e82
xiara125 <86992014+xiara125@users.noreply.github.com>
2024-06-15 20:50:58 +0900

    Day 27

---
+++ [DAY 27] 2024.06.15/[Beginner] 1512. Number of Good Pairs.py
@@ -0,0 +1,9 @@
+# [43ms] Beats 15.60%
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(0,len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    count = count + 1
+        return count
